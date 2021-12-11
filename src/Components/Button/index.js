dd773d1dc4bccd290de83396ee2b7a8c5ae6e349
Hashir-Khan777 <khnhashir555@gmail.com>
2021-12-11 15:10:32 +0500

    add components

---
+++ src/Components/Button/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/index.css";
+
+const Button = () => {
+  return (
+    <div>
+      <button>submit</button>
+    </div>
+  );
+};
+
+export default Button;
